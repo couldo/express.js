419c72c5a8b1f5e4a2d455ca934e0176e6b80d53
Vitor Lobão Fernandes <vitorl@LKDTI007.(none)>
2012-07-20 12:49:49 -0300

    Ninja Store

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+  
+var store = require('./routes/store');
+var error = require('./routes/error');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', store.home);
+app.post('/', store.home_post_handler);
+// display the list of item
+app.get('/items', store.items);
+// show individual item
+app.get('/item/:id', store.item);
+// show general pages
+app.get('/page', store.page);
+app.get('/logout', function(req, res) {
+    // delete the session variable
+    delete req.session.username;
+    // redirect user to homepage
+    res.redirect('/');
+});
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/admin.js
@@ -0,0 +1,3 @@
+exports.index = function(req, res){
+  res.render('adm-home', { title: 'LojasKD' })
+};
